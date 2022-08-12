b7270e6acfcae37889620820e4722c413a1477cf
Apoorv Srivastava <110709655+apoorvsrivastava58@users.noreply.github.com>
2022-08-12 06:09:46 +0530

    Update snake.js

--- snake.js
+++ snake.js
@@ -12,13 +12,13 @@ let snake = [
 ]
 
 let score = 0;
-// True if changing direction
+
 let changing_direction = false;
-// Horizontal velocity
+
 let food_x;
 let food_y;
 let dx = 10;
-// Vertical velocity
+
 let dy = 0;
 
 
@@ -33,7 +33,7 @@ gen_food();
 
 document.addEventListener("keydown", change_direction);
 
-// main function called repeatedly to keep the game running
+
 function main() {
 
     if (has_game_ended()) return;
@@ -49,21 +49,21 @@ function main() {
   }, 100)
 }
 
-// draw a border around the canvas
+
 function clear_board() {
-  //  Select the colour to fill the drawing
+  
   snakeboard_ctx.fillStyle = board_background;
-  //  Select the colour for the border of the canvas
+  
   snakeboard_ctx.strokestyle = board_border;
-  // Draw a "filled" rectangle to cover the entire canvas
+  
   snakeboard_ctx.fillRect(0, 0, snakeboard.width, snakeboard.height);
-  // Draw a "border" around the entire canvas
+  
   snakeboard_ctx.strokeRect(0, 0, snakeboard.width, snakeboard.height);
 }
 
-// Draw the snake on the canvas
+
 function drawSnake() {
-  // Draw each part
+  
   snake.forEach(drawSnakePart)
 }
 
@@ -74,17 +74,17 @@ function drawFood() {
   snakeboard_ctx.strokeRect(food_x, food_y, 10, 10);
 }
 
-// Draw one snake part
+
 function drawSnakePart(snakePart) {
 
-  // Set the colour of the snake part
+  
   snakeboard_ctx.fillStyle = snake_col;
-  // Set the border colour of the snake part
+  
   snakeboard_ctx.strokestyle = snake_border;
-  // Draw a "filled" rectangle to represent the snake part at the coordinates
-  // the part is located
+  
+
   snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-  // Draw a border around the snake part
+  
   snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
 
@@ -104,11 +104,11 @@ function random_food(min, max) {
 }
 
 function gen_food() {
-  // Generate a random number the food x-coordinate
+  
   food_x = random_food(0, snakeboard.width - 10);
-  // Generate a random number for the food y-coordinate
+  
   food_y = random_food(0, snakeboard.height - 10);
-  // if the new food location is where the snake currently is, generate a new food location
+  
   snake.forEach(function has_snake_eaten_food(part) {
     const has_eaten = part.x == food_x && part.y == food_y;
     if (has_eaten) gen_food();
@@ -121,7 +121,7 @@ function change_direction(event) {
   const UP_KEY = 38;
   const DOWN_KEY = 40;
   
-// Prevent the snake from reversing
+
 
   if (changing_direction) return;
   changing_direction = true;
@@ -151,7 +151,7 @@ function change_direction(event) {
 function move_snake() {
   // Create the new Snake's head
   const head = {x: snake[0].x + dx, y: snake[0].y + dy};
-  // Add the new head to the beginning of snake body
+  
   snake.unshift(head);
   const has_eaten_food = snake[0].x === food_x && snake[0].y === food_y;
   if (has_eaten_food) {
